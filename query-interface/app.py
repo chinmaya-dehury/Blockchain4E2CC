75aa60251950d7fc961c0ae10c38bcf0d1f7b0d5
Iwada Eja <iwada.eja@finnair.com>
2023-05-08 19:13:53 +0300

    Minor Query Interface changes

--- query-interface/app.py
+++ query-interface/app.py
@@ -36,8 +36,7 @@ def objects(bucket_name):
 
 @app.route('/objects/<bucket_name>/<path:object_name>', endpoint='object_content')
 def object_content(bucket_name, object_name):
-    new_info = session.pop('new_info', None)
-    #if new_info is not None:
+    verified = session.pop('verified', None)
     try:
         data = minio_client.get_object(bucket_name, object_name)
         content = data.read().decode('utf-8')
@@ -45,26 +44,23 @@ def object_content(bucket_name, object_name):
     except S3Error as e:
         print(e)
         content = ''
-    return render_template('object_content.html', object_name=object_name, data=data, new_info=new_info)
+    return render_template('object_content.html', object_name=object_name, data=data, verified=verified)
 
 @app.route('/verify/<string:id>', methods=['GET', 'POST'])
 def verify(id):
     if request.method == 'POST':
         data = request.form['data']
-        #print("BEOFRE CONVERSION",data)
-        #data_str = json.dumps(data)
         new_string = data.replace("'", "\"")
         new_string = new_string.replace(' ', '').replace('\t', '').replace('\n', '')
        # print("JSON AFTER CONVERSION",new_string)
-        result = query_contract('tartucitycouncilchannel', 'tartucitycouncil', [new_string, id])
-        print("ID", id)
-        print("New String", new_string)
-        print("Returned Result:::", result)
+        response_dict = query_contract('tartucitycouncilchannel', 'tartucitycouncil', [new_string, id])
+        success = response_dict['response']['success']
+        if success:
+            session['verified'] = True
+        else:
+            session['verified'] = False
         previous_path = request.referrer
-        session['new_info'] = 'some_value'
-        print(previous_path)
         return redirect(previous_path)
-        #make_primary_blockchain_call(json_data)
     else:
         return redirect(request.url)
 
@@ -94,11 +90,6 @@ def query_contract(contract_name, contract_method, args):
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()
-
-contract_name = 'tartucitycouncilchannel'
-contract_method = 'tartucitycouncil'
-data = data = '{"temperature":12.542256603183866,"timestamp":"2023-05-08T11:28:01Z","org":"TartuCityCouncil","device":"sensorOne","id":"65c8ce38-5466-4de7-ac08-a663cc26b644","arrivalTime":"2023-05-08T11:28:02.537+03:00","departTimeFromFogNode":"2023-05-08T11:28:02.539+03:00"}'
-args = [data, '65c8ce38-5466-4de7-ac08-a663cc26b644']
 
 if __name__ == '__main__':
     app.run()
